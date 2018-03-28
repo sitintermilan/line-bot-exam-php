6bd3a3bf41e22a2cded0da35caf9ab4a37c5c93b
sitintermilan <sitintermilan@gmail.com>
2018-03-28 16:14:05 +0700

    test file log-userid
    test file log-userid

--- webhooks.php
+++ webhooks.php
@@ -19,7 +19,11 @@ if (!is_null($events['events'])) {
 			$text = $event['source']['userId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-
+$strFileName = "log-userid.php";
+$objFopen = fopen($strFileName, 'a');
+$strText1 = "\n\r".$text;
+fwrite($objFopen, $strText1);
+fclose($objFopen);
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
